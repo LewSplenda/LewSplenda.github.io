25c39910892ee2bb10048483d0fff0e69376e4b4
LewSplenda <92895314+LewSplenda@users.noreply.github.com>
2022-10-05 16:16:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let instagram = document.getElementById("instagram")
+let facebook = document.getElementById("facebook")
+let whatnot = document.getElementById("whatnot")
+
+instagram.addEventListener("click", function(){
+    openInNewTab('www.google.com')
+})
